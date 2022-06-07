6a1ac85596f0c55d39ff37f36570ae006c547bc9
James <jimibue@gmail.com>
2022-06-07 11:54:51 -0600

    lots of work done

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,17 @@ Rails.application.routes.draw do
   namespace :api do 
     # resources :dishes
     get '/dishes', to: 'dishes#index' # return all dishes
-    post '/dishes', to: 'dishes#create' # create a dish
+    post '/dishes', to: 'dishes#create' # create a dish {name(required), price, descrption}
     get '/dishes/:id', to:'dishes#show' # return 1 dish
-    put '/dishes/:id', to:'dishes#update' # update 1 dish
-    delete '/dishes/:id', to:'dishes#destroy' # destroy 1 dishs
+    put '/dishes/:id', to:'dishes#update' # update 1 dish {name(required), price, descrption}
+    delete '/dishes/:id', to:'dishes#destroy' # destroy 1 dish
   end
 end
+
+# get '/dishes',  # return all dishes
+# post '/dishes',  # create a dish {name(required), price, descrption}
+# get '/dishes/:id',  # return 1 dish
+# put '/dishes/:id',  # update 1 dish {name(required), price, descrption}
+# delete '/dishes/:id',  # destroy 1 dish
+
+#LOTS OF CHANGES BUT NOT DONE WITH FEATURE
